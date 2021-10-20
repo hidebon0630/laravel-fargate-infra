3474ed0cf214ed3ea612651ffcdabe25adeec224
hidebon0630 <hyagi0630@icloud.com>
2021-10-20 19:39:09 +0900

    6.9 ALIAS レコードの作成

--- envs/prod/routing/appfoobar_link/route53.tf
+++ envs/prod/routing/appfoobar_link/route53.tf
@@ -1,3 +1,17 @@
 resource "aws_route53_zone" "example" {
   name = "g0pf.com"
 }
+
+resource "aws_route53_record" "root_a" {
+  count = var.enable_alb ? 1 : 0
+
+  name    = aws_route53_zone.example.name
+  type    = "A"
+  zone_id = aws_route53_zone.example.zone_id
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_lb.this[0].dns_name
+    zone_id                = aws_lb.this[0].zone_id
+  }
+}
